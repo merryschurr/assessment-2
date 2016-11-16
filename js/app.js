c7bd3bdeed4007f5163c33cb635caafe05bfd487
merryschurr <merryschurr@gmail.com>
2016-11-15 21:00:51 -0700

    adds JS

--- js/app.js
+++ js/app.js
@@ -1,75 +1,38 @@
-window.onkeydown = function (e) {
+window.keydown = function (e) {
 if (!e) {
 e = window.event;
 }
+}
 
-var code = e.keyCode;
-var orange = document.getElementById("orange");
-var blue = document.getElementById("blue");
-var white = document.getElementById("white");
-
-var left = parseInt (orange.style.left, 10);
-    if ( code == 37 ) { //LEFT
+var orange = document.getElementById("playerOne");
+var blue = document.getElementById("playerTwo");
 
 
-    if ( left > 0 ) {
-    orange.style.left = left - 10 + 'px';
+var Xpos = 0; //Sets starting position left/right
+var Ypos = 0; //Sets starting position up/down
+var spd = 250; //Milliseconds
+var dstnc = 100; //Pixels
+
+console.log("hello");
+
+$(document).keydown(function(e){
+    if (e.keyCode == 39){ // arrow right
+        $('#playerOne').animate({
+            left: dstnc+Xpos+'px'
+        }, spd );
+        Xpos = Xpos+dstnc;
     }
-    } else if ( code == 39 ) { //RIGHT
+    if (e.keyCode == 68){ //D Right
+        $('#playerTwo').animate({
+            left: dstnc+Xpos+'px'
+        }, spd );
+        Xpos = Xpos+dstnc;
+    }
+});
 
-
-    if ( left+orange.width+10 < window.innerWidth ) {
-    orange.style.left = left + 10 + 'px';
-    }
-    } else if ( code == 40 ) { //DOWN
-    if ( top+orange.height+10 < window.innerHeight ) {
-    orange.style.top = top + 10 +'px';
-    }
-    }
-    }
-
-
-var leftTwo = parseInt (blue.style.leftTwo, 10);
-    if ( code == 37 ) { //LEFT
-
-    if ( leftTwo > 0 ) {
-    blue.style.leftTwo = leftTwo - 10 + 'px';
-    }
-    } else if ( code == 39 ) { //RIGHT
-
-    if ( leftTwo+blue.width+10 < window.innerWidth ) {
-    blue.style.leftTwo = leftTwo + 10 + 'px';
-    }
-    } else if ( code == 40 ) { //DOWN
-    if ( top+blue.height+10 < window.innerHeight ) {
-    blue.style.top = top + 10 +'px';
-    }
-    }
-    }
-
-var leftThree = parseInt (blue.style.leftThree, 10);
-
-    if ( code == 37 ) { //LEFT
-
-
-    if ( left > 0 ) {
-    white.style.left = left - 10 + 'px';
-    }
-    } else if ( code == 38 ) { //UP
-  
-    if ( top > 0 ) {
-    white.style.top = top - 10 + 'px';
-    }
-    } else if ( code == 39 ) { //RIGHT
-
-
-    if ( left+white.width+10 < window.innerWidth ) {
-    white.style.left = left + 10 + 'px';
-    }
-    } else if ( code == 40 ) { //DOWN
-    if ( top+white.height+10 < window.innerHeight ) {
-    white.style.top = top + 10 +'px';
-    }
-    }
-    }
-
+$(window).keypress(function (e) {
+  if (e.keyCode === 39*3|| e.keyCode === 68*3) {
+    e.preventDefault()
+    console.log('Winner')
+  }
+})
